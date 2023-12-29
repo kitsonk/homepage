8467c2d66026d84297ed22937f85e6fc52bb2483
Kitson Kelly <me@kitsonkelly.com>
2023-12-29 20:16:03 +1100

    chore: update dependencies

--- fresh.gen.ts
+++ fresh.gen.ts
@@ -2,32 +2,34 @@
 // This file SHOULD be checked into source version control.
 // This file is automatically updated during development when running `dev.ts`.
 
-import * as $0 from "./routes/_404.tsx";
-import * as $1 from "./routes/_app.tsx";
-import * as $2 from "./routes/_middleware.ts";
-import * as $3 from "./routes/about.tsx";
-import * as $4 from "./routes/blog.tsx";
-import * as $5 from "./routes/contact.tsx";
-import * as $6 from "./routes/index.tsx";
-import * as $7 from "./routes/posts/[id].tsx";
-import * as $8 from "./routes/posts/content.ts";
-import * as $9 from "./routes/resume-cv.tsx";
+import * as $_404 from "./routes/_404.tsx";
+import * as $_app from "./routes/_app.tsx";
+import * as $_middleware from "./routes/_middleware.ts";
+import * as $about from "./routes/about.tsx";
+import * as $blog from "./routes/blog.tsx";
+import * as $contact from "./routes/contact.tsx";
+import * as $index from "./routes/index.tsx";
+import * as $posts_id_ from "./routes/posts/[id].tsx";
+import * as $posts_content from "./routes/posts/content.ts";
+import * as $resume_cv from "./routes/resume-cv.tsx";
+
+import { type Manifest } from "$fresh/server.ts";
 
 const manifest = {
   routes: {
-    "./routes/_404.tsx": $0,
-    "./routes/_app.tsx": $1,
-    "./routes/_middleware.ts": $2,
-    "./routes/about.tsx": $3,
-    "./routes/blog.tsx": $4,
-    "./routes/contact.tsx": $5,
-    "./routes/index.tsx": $6,
-    "./routes/posts/[id].tsx": $7,
-    "./routes/posts/content.ts": $8,
-    "./routes/resume-cv.tsx": $9,
+    "./routes/_404.tsx": $_404,
+    "./routes/_app.tsx": $_app,
+    "./routes/_middleware.ts": $_middleware,
+    "./routes/about.tsx": $about,
+    "./routes/blog.tsx": $blog,
+    "./routes/contact.tsx": $contact,
+    "./routes/index.tsx": $index,
+    "./routes/posts/[id].tsx": $posts_id_,
+    "./routes/posts/content.ts": $posts_content,
+    "./routes/resume-cv.tsx": $resume_cv,
   },
   islands: {},
   baseUrl: import.meta.url,
-};
+} satisfies Manifest;
 
 export default manifest;
